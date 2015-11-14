454fb7b411cf47e4262eefad685c286ad477fb0a
Hayato Kimura <prprhyt@outlook.com>
2015-11-14 16:00:14 +0900

    カードを生成できるようにした。

--- app/src/main/java/example/com/bingocard/MainActivity.java
+++ app/src/main/java/example/com/bingocard/MainActivity.java
@@ -7,31 +7,59 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
+
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
+    Boolean[][] celltrueisAlive = new Boolean[5][5];
     Button[][] btn = new Button[5][5];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         for(int i=0;i<5;i++) {
+            Random rnd = new Random();
+            int rndnum[] = {0,0,0,0,0};
+            for(int k=0;k<5;) {
+                int temprnd=rnd.nextInt(15)+i*15+1;
+
+                boolean trueisok = true;
+                    for(int l=0;l<5;l++) {
+                        if (rndnum[l] == temprnd) {
+                            trueisok = false;
+                            break;
+                        }
+                    }
+                if(trueisok==true){
+                    rndnum[k]=temprnd;
+                    k++;
+                }
+            }
             for(int j=0;j<5;j++) {
                 String btnidstr= "btn_"+Integer.toString(i)+"_"+Integer.toString(j);
                 int viewId = getResources().getIdentifier(btnidstr,"id",getPackageName());
                 btn[i][j] = (Button)findViewById(viewId);
                 btn[i][j].setOnClickListener(this);
+                celltrueisAlive[i][j]=true;
+                celltrueisAlive[2][2]=false;
+                btn[i][j].setText(Integer.toString(rndnum[j]));
             }
         }
-    }
+        btn[2][2].setText("Free");
 
+    }
 
         public void onClick(View v){
             for(int i=0;i<5;i++){
                 for(int j=0;j<5;j++){
                     String btnidstr = "btn_"+Integer.toString(i)+"_"+Integer.toString(j);
                     int viewId = getResources().getIdentifier(btnidstr,"id",getPackageName());
-                    if(v.getId()==viewId){
-                        btn[i][j].setText(Integer.toString(i)+Integer.toString(j));
+                    if( v.getId()==viewId){
+                        if(celltrueisAlive[i][j]==true) {
+                            btn[i][j].setText(Integer.toString(i) + Integer.toString(j));
+                            celltrueisAlive[i][j] = false;
+                        }
                         break;
                     }
                 }
