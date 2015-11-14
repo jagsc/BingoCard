2e883d8579465713c9619a62b305184b75285ab3
Hayato Kimura <prprhyt@outlook.com>
2015-11-14 14:43:07 +0900

    Buttonとクリックイベントを関連づけた

--- app/src/main/java/example/com/bingocard/MainActivity.java
+++ app/src/main/java/example/com/bingocard/MainActivity.java
@@ -4,16 +4,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
+    Button[][] btn = new Button[5][5];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int i=0;i<5;i++) {
+            for(int j=0;j<5;j++) {
+                String btnidstr= "btn_"+Integer.toString(i)+"_"+Integer.toString(j);
+                int viewId = getResources().getIdentifier(btnidstr,"id",getPackageName());
+                btn[i][j] = (Button)findViewById(viewId);
+                btn[i][j].setOnClickListener(this);
+            }
+        }
     }
 
-    @Override
+
+        public void onClick(View v){
+            for(int i=0;i<5;i++){
+                for(int j=0;j<5;j++){
+                    String btnidstr = "btn_"+Integer.toString(i)+"_"+Integer.toString(j);
+                    int viewId = getResources().getIdentifier(btnidstr,"id",getPackageName());
+                    if(v.getId()==viewId){
+                        btn[i][j].setText(Integer.toString(i)+Integer.toString(j));
+                        break;
+                    }
+                }
+            }
+        }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
