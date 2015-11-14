63f87a2703da52967febb5050f7c814430f42529
Hayato Kimura <prprhyt@outlook.com>
2015-11-14 18:19:32 +0900

    リーチとビンゴの判定をつけた(一部想定がの動作あり)

--- app/src/main/java/example/com/bingocard/MainActivity.java
+++ app/src/main/java/example/com/bingocard/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -13,17 +14,34 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     Boolean[][] celltrueisAlive = new Boolean[5][5];
+
+    Boolean[] cellcolumntrueisreach = new Boolean[5];
+    Boolean[] cellrowtrueisreach = new Boolean[5];
+    Boolean[]cellcolumntrueisbingo = new Boolean[5];
+    Boolean[]cellrowtrueisbingo = new Boolean[5];
+    Boolean[]cellcrosstrueisreach = new Boolean[2];
+    Boolean[]cellcrosstrueisbingo = new Boolean[2];
     Button[][] btn = new Button[5][5];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cellcrosstrueisreach[0]=false;
+        cellcrosstrueisbingo[0]=false;
+        cellcrosstrueisreach[1]=false;
+        cellcrosstrueisbingo[1]=false;
         for(int i=0;i<5;i++) {
-            Random rnd = new Random();
+            cellcolumntrueisreach[i]=false;
+            cellrowtrueisreach[i]=false;
+            cellcolumntrueisbingo[i]=false;
+            cellrowtrueisbingo[i]=false;
             int rndnum[] = {0,0,0,0,0};
             for(int k=0;k<5;) {
+                long seed = System.currentTimeMillis();
+                Random rnd = new Random(seed);
                 int temprnd=rnd.nextInt(15)+i*15+1;
-
                 boolean trueisok = true;
                     for(int l=0;l<5;l++) {
                         if (rndnum[l] == temprnd) {
@@ -34,6 +52,11 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
                 if(trueisok==true){
                     rndnum[k]=temprnd;
                     k++;
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
             for(int j=0;j<5;j++) {
@@ -47,8 +70,8 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             }
         }
         btn[2][2].setText("Free");
-
     }
+
 
         public void onClick(View v){
             for(int i=0;i<5;i++){
@@ -57,14 +80,106 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
                     int viewId = getResources().getIdentifier(btnidstr,"id",getPackageName());
                     if( v.getId()==viewId){
                         if(celltrueisAlive[i][j]==true) {
-                            btn[i][j].setText(Integer.toString(i) + Integer.toString(j));
+                            btn[i][j].setText("OK");
                             celltrueisAlive[i][j] = false;
+                            judge_cellstate(i,j);
                         }
                         break;
                     }
                 }
             }
         }
+
+    public void judge_cellstate(int x,int y){
+        int cellAlivecount=0;
+        for(int i=0;i<5;i++){
+            if(celltrueisAlive[i][y]==false){
+                cellAlivecount++;
+            }
+        }
+        if(cellAlivecount==4){
+            if(cellrowtrueisreach[y]==false) {
+                /*ここにアニメーションを挿入*/
+                String tststr = Integer.toString(y);
+                Toast.makeText(this, "Reach!y:"+tststr, Toast.LENGTH_LONG).show();
+                cellrowtrueisreach[y] = true;
+            }
+        }
+        if(cellAlivecount==5){
+            if(cellrowtrueisbingo[y]==false){
+                /*ここにアニメーションを挿入*/
+                String tststr = Integer.toString(y);
+                Toast.makeText(this, "Bingo!y:"+tststr, Toast.LENGTH_LONG).show();
+                cellrowtrueisbingo[y] = true;
+            }
+        }
+
+        cellAlivecount=0;
+        for(int i=0;i<5;i++){
+            if(celltrueisAlive[4-i][i]=false){
+                cellAlivecount++;
+            }
+        }
+        if(cellAlivecount==4){
+            if(cellcrosstrueisreach[1]==false){
+                String tststr = Integer.toString(1);
+                Toast.makeText(this, "Reach!crs:"+tststr, Toast.LENGTH_LONG).show();
+                cellcrosstrueisreach[1]=true;
+            }
+        }
+        if(cellAlivecount==5){
+            if(cellcrosstrueisbingo[1]==false){
+                String tststr = Integer.toString(1);
+                Toast.makeText(this, "Bingo!crs:"+tststr, Toast.LENGTH_LONG).show();
+                cellcrosstrueisbingo[1]=true;
+            }
+        }
+
+
+        cellAlivecount=0;
+        for(int i=0;i<5;i++){
+            if(celltrueisAlive[i][i]=false){
+                cellAlivecount++;
+            }
+        }
+        if(cellAlivecount==4){
+            if(cellcrosstrueisreach[0]==false){
+                String tststr = Integer.toString(0);
+                Toast.makeText(this, "Reach!crs:"+tststr, Toast.LENGTH_LONG).show();
+                cellcrosstrueisreach[0]=true;
+            }
+        }
+        if(cellAlivecount==5){
+            if(cellcrosstrueisbingo[0]==false){
+                String tststr = Integer.toString(0);
+                Toast.makeText(this, "Bingo!crs:"+tststr, Toast.LENGTH_LONG).show();
+                cellcrosstrueisbingo[0]=true;
+            }
+        }
+
+        cellAlivecount=0;
+        for(int j=0;j<5;j++){
+            if(celltrueisAlive[x][j]==false){
+                cellAlivecount++;
+            }
+        }
+        if(cellAlivecount==4){
+            if(cellcolumntrueisreach[x]==false){
+                /*ここにアニメーションを挿入*/
+                String tststr = Integer.toString(x);
+                Toast.makeText(this, "Reach!x:"+tststr, Toast.LENGTH_LONG).show();
+                cellcolumntrueisreach[x]=true;
+            }
+        }
+        if(cellAlivecount==5){
+            if(cellcolumntrueisbingo[x]==false){
+                /*ここにアニメーションを挿入*/
+                String tststr = Integer.toString(x);
+                Toast.makeText(this, "Bingo!x:"+tststr, Toast.LENGTH_LONG).show();
+                cellrowtrueisbingo[x] = true;
+            }
+        }
+    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
